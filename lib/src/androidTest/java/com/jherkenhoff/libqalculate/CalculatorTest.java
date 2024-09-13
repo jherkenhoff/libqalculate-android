510b564f2cadba629a4b77352bb3235f38e19ebf
jherkenhoff <jherkenhoff+github@mailbox.org>
2024-09-14 01:04:13 +0200

    Add unit test. Update gradle veersion

---
+++ lib/src/androidTest/java/com/jherkenhoff/libqalculate/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.jherkenhoff.libqalculate;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Before
+    public void loadLibrary() {
+        System.loadLibrary("qalculate_swig");
+    }
+    @Test
+    public void testBasicCalculation() {
+        Calculator calc = new Calculator();
+        String res = calc.calculateAndPrint("1+1");
+        assertEquals(res, "2");
+    }
+}
